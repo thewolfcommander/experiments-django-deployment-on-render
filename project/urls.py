0ebb42bf9d3d4c21f726988717a1847b89ba1df1
Manoj Tyag <manoj.tyagi@limechat.ai>
2023-02-07 19:07:16 +0530

    added a sample endpoint

--- project/urls.py
+++ project/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from .views import TestAPIView
+
 urlpatterns = [
+    path('', TestAPIView.as_view()),
     path('admin/', admin.site.urls),
 ]
